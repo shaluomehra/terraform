f2c052c33fae5b762cc7aed77d3f87709af602fa
Shaluo Mehra <smehra@spartaglobal.com>
2023-10-20 10:20:18 +0100

    Mark down added

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 
 
 # location of aws
-   region = var.aws-region #"eu-west-1"
+   region = var.aws-region
  
 }
 # to download required dependencies
 # create a service/resource on the cloud - ec2 on AWS
 
 resource "aws_instance" "shaluo-tech254-iac" {
-    ami = var.web-app_ami_id #"ami-0f31088b1d3e180d3"
+    ami = var.web-app_ami_id
     instance_type = "t2.micro"
     tags = {
      Name = "shaluo-tech254-iac"
